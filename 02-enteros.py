cbbf6410111a674da2ce250addc3b029787fac6b
Nouvellie <remains_of_hate@hotmail.com>
2016-12-27 00:06:20 -0300

    enteros, reales, operadores

---
+++ 02-enteros.py
@@ -0,0 +1,31 @@
+
+#int numeros standard
+#long el numero q sea (Grande) requiere mayor memoria del sistema
+
+#e = 5
+#e = 5L # ahora es variable long
+
+#real = 0.567
+#real = 0,56e-3
+
+#print real
+
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+# SUMA
+print a + b
+# RESTA
+print c - a
+# MULTIPLICACION
+print d * c
+# EXPONENTE
+print c ** 2
+# DIVISION
+print float(c) / a #con el float hacemos q nos de los decimales al tener 1 numero real
+# DIVISION ENTERA (Sin decimales)
+print c / a
+# MODULO
+print 7 % 3
