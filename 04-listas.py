17bc54aa50caf3a70e41b55d2cb45e33cae3828e
Nouvellie <remains_of_hate@hotmail.com>
2016-12-27 00:18:52 -0300

    listas created

---
+++ 04-listas.py
@@ -0,0 +1,24 @@
+l = [2, "tres", True, ["uno",10]]
+
+l2 = l[1]
+
+l3 = l[3][0]
+
+#Cambiar contenido
+
+l[1] = 4
+
+#Imprime solo los primeros 3 elementos
+l2 = l[0:3]
+
+#Imprime los 3 primeros elementos saltandose de 2 en 2
+l3 = l[0:3:2]
+
+print l
+
+print l2
+
+print l3
+
+print l[1]
+
